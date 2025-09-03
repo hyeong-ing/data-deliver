3b4bf35fbe13c51b88d24f7d7a5c59659603e8b8
hyeyeongAn <mybymine64@naver.com>
2025-09-03 17:41:29 +0900

    props와 emit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script>
+import ChildComponent from "@/components/ChildComponent.vue";
+
+export default {
+  name: "App",
+  components: {ChildComponent},
+  data() {
+    return {open:false, backColor:'lemonchiffon'}
+  },
+  methods: {
+    show(color) {
+      this.backColor = color;
+    },
+    child() {
+      this.open = !this.open;
+    }
+  }
+}
+</script>
+
+
+
+<template>
+<div class="app" :style="{backgroundColor: backColor}">
+  <button class="p-btn" @click="child"> 자식 컴포넌트 나타나기 </button>
+  <ChildComponent :open="open" @show="show"/>
+</div>
+</template>
+
+
+
+<style>
+.app {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+}
+
+.p-btn {
+  position: absolute;
+  width: 30%;
+  height: 10%;
+  top: 50%;
+  left: 75%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  font-size: 1.1rem;
+  z-index: 2;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+  name: 'ChildComponent',
+  // 부모에게 데이터를 받는 곳
+  props: {
+    open: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    showToText() {
+      this.$emit('show', 'lightpink')
+    }
+  },
+}
+</script>
+
+
+
+
+<template>
+  <div class="child" :class="{open:open}">
+    <button @click="showToText()" class="c-btn"> 부모 컴포넌트 색상 바꾸기 </button>
+  </div>
+
+</template>
+
+
+
+
+<style>
+.child {
+  position: relative;
+  width: 50vw;
+  height: 100vh;
+  top: 0;
+  right:0;
+  background-color: cornflowerblue;
+  transform: translateX(-100%);
+  transition: transform 0.4s cubic-bezier(.71,1.7,.58,.98);
+}
+
+.child.open {
+  transform: translateX(0%);
+}
+
+.c-btn {
+  position: absolute;
+  width: 60%;
+  height: 10%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  font-size: 1.1rem;
+  cursor: pointer;
+}
+
+</style>
